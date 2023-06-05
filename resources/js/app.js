ec64606c44bb843d8f636145ac9d7ad2ab67e4be
omar <omar@Macs-MacBook-Pro.local>
2023-06-05 21:16:59 +0100

    configure vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import feather from "feather-icons";
+import { createApp } from "vue";
+import App from "@/Components/App.vue";
+
+createApp(App).mount("#app");
+
+feather.replace();
